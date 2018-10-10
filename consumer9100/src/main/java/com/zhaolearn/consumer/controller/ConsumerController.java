51ed38d63ab346f5b724090bbf65689cab5eb5cd
RyzeZhao <643080112@qq.com>
2018-10-10 11:10:36 +0800

    Consumer9100结合SpringCloudBus RabbitMQ

--- consumer9100/src/main/java/com/zhaolearn/consumer/controller/ConsumerController.java
+++ consumer9100/src/main/java/com/zhaolearn/consumer/controller/ConsumerController.java
@@ -3,11 +3,13 @@ package com.zhaolearn.consumer.controller;
 import com.zhaolearn.consumer.service.ConsumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConsumerController {
 
     @Autowired
