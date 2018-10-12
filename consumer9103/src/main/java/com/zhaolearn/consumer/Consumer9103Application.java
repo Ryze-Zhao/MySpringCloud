2543d9391b54f4f524dd553f3efa4818c2adcba4
RyzeZhao <643080112@qq.com>
2018-10-12 17:33:53 +0800

    修改dashboard8777整合监控聚合，并监控Consumer-Ribbon与Consumer-Feign

--- consumer9103/src/main/java/com/zhaolearn/consumer/Consumer9103Application.java
+++ consumer9103/src/main/java/com/zhaolearn/consumer/Consumer9103Application.java
@@ -1,9 +1,12 @@
 package com.zhaolearn.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +15,14 @@ public class Consumer9103Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Consumer9103Application.class, args);
+    }
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
