03fd2906e886cf07bca4cd740bc36ee4e4bd3e5e
RyzeZhao <643080112@qq.com>
2018-10-13 10:00:00 +0800

    合并EurekaHAC，Eureka端口8761,8762,8763，Zuul增加对provide的访问

---
+++ eureka8763/src/main/java/com/zhaolearn/eureka/Eureka8763Application.java
@@ -0,0 +1,14 @@
+package com.zhaolearn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka8763Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka8763Application.class, args);
+    }
+}
